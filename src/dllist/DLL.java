0edd5428d471dedb2222586ebf030454f7a54ace
anthony <ar81@stmarys-ca.edu>
2024-11-06 14:34:15 -0800

    Imported from IntelliJ to VSCode
    Literally just moved all my files into the project folder. Tell me if there are any issues.

---
+++ src/dllist/DLL.java
@@ -0,0 +1,75 @@
+package dllist;
+
+public class DLL<T> {
+    /*
+    You can and should base your code off LL<T> and Node<T> which we did in class and the lab. The package these files live in should be called “dllist”
+
+    The DLL<T> class should have 3 fields: size (int), head (DLNode<T>) and tail (DLNode<T>)
+     */
+
+
+
+    public DLL() {
+        // TODO: Implement
+    }
+
+    public DLL(DLNode<T> node) {
+        // TODO: Implement
+    }
+
+    public void copyOver(DLNode<T> node) {
+        // TODO: Implement
+    }
+
+    public DLNode<T> getNode(int idx) {
+        return null;    // TODO: Implement
+    }
+
+    public boolean isEmpty() {
+        return false;   // TODO: Implement
+    }
+
+    public void makeEmpty() {
+        // TODO: Implement
+    }
+
+    public int size() {
+        return 0;   // TODO: Implement
+    }
+
+    public String toString() {
+        return null;    // TODO: Implement
+    }
+
+    public int find(T x) {
+        return 0;   // TODO: Implement
+    }
+
+    public void insert(int idx, T data) {
+        // TODO: Implement
+    }
+
+    public void delete(int idx) {
+        // TODO: Implement
+    }
+
+    public void remove(T data) {
+        // TODO: Implement
+    }
+
+    public void append(T data) {
+        // TODO: Implement
+    }
+
+    public DLNode<T> getHead() {
+        return null;    // TODO: Implement
+    }
+
+    public int hashCode() {
+        return 0;   // TODO: Implement
+    }
+
+    public boolean equals(Object obj) {
+        return false;   // TODO: Implement
+    }
+}
---
+++ src/dllist/DLNode.java
@@ -0,0 +1,5 @@
+package dllist;
+
+public class DLNode<T> {
+
+}

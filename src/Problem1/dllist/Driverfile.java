c7bd7559cc9b9268119285c4b340469c2b312f16
melanie-maira <mrp13@stmarys-ca.edu>
2024-11-19 10:07:58 -0800

    start of driver file

---
+++ src/Problem1/dllist/Driverfile.java
@@ -0,0 +1,16 @@
+package dllist;
+
+public class Driverfile {
+
+    public void addoddback(){
+
+    }
+
+    public void addevenfront(){ 
+
+    }
+
+    public void thirdremove(){ 
+        
+    }
+}

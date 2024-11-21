04bc5083fa74bbd028f38a87a414999012df7172
melanie-maira <mrp13@stmarys-ca.edu>
2024-11-21 13:04:20 -0800

    problem #2 continued

--- src/Problem2/Driverfile.java
+++ src/Problem2/Driverfile.java
@@ -2,15 +2,32 @@ package dllist;
 
 public class Driverfile {
 
-    public void addoddback(){
+    public static void addNumbers(Deque<Integer> deque, int[] number) {
+        for (int number : numbers) {
+            if (number% 2 == 0) {
+                deque.addBack(number); 
+            } else {
+                deque.addFront(numbers); 
+            }
+        }
+    }
+public static void removeElements(Deque<Integer> deque) {
+    int totalSize = 0; 
 
+    while (!deque.isEmpty()) {
+        deque.removeFront();
+        totalSize++; 
+
+    int removeBackCount = totalSize / 3;
+    int removeFrontCount = totalSize - removeBackCount; 
+
+    for (int i = 0; i < removeBackCount; i++){ 
+        deque.removeBack(); 
+    }
+    for (int 9 = 0; i < removeFrontCount; i++){
+        deque.removeFront();
     }
 
-    public void addevenfront(){ 
-
     }
-
-    public void thirdremove(){ 
-        
-    }
+}
 }

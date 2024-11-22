4c6053794d597f1c89fb83f72bf6a20627d282d0
anthony <ar81@stmarys-ca.edu>
2024-11-22 15:23:16 -0800

    IMPORTANT: PLEASE READ MY COMMENTS
    I checked the code for Problem 2 and everything is broken. I left comments on how they can be fixed. Please let me know once everything is changed.

--- src/Problem2/Deque.java
+++ src/Problem2/Deque.java
@@ -1,7 +1,16 @@
 package deque;
 
+// This file should be importing DLL file from Problem 1.
+// This class should be using a generic T parameter and not a string.
+
 public class Deque<string> {
-    
+
+    // Deque should be using DLL<T> list.
+        // Like this:   private DLL<T> list;
+    // Without it, head and tail remain undefined.
+    // Make a zero-parameter that defines "list" as a new DLL<> object.
+
+    // This can be done using "insert" with index position 0.
     public void addFront(T data) { 
         if (isEmpty()){
             head = tail = newNode;
@@ -13,6 +22,7 @@ public class Deque<string> {
         idx++; 
     }
 
+    // This can be done using "append".
     public void addBack(T data) {
         Node newNode = new Node(data); 
         if (isEmpty()) {
@@ -27,6 +37,7 @@ public class Deque<string> {
 
     }
 
+    // This can be done using "getHead().data" and "delete".
     public T removeFront() {
         if (isEmpty()) throw new IllegalStateException("Deque is empty"); 
         T data = head.data; 
@@ -41,6 +52,7 @@ public class Deque<string> {
         }
 
 
+    // This can be done using "getNode(list.size() -1).data" and "delete".
     public void removeBack() { 
         if (isEmpty()) throw new IllegalStateException("Deque is empty");
         T data = tail.data;
@@ -54,19 +66,33 @@ public class Deque<string> {
         return data;
         }
 
+    // This is fine, but can also be done using "list.isEmpty()".
+        // That is a method created in the DLL file.
     public boolean isEmpty() {
         return size == 0; 
     }
 
+    // This is fine, but can also be done usign "list.makeEmpty()".
+        // That is a method created in the DLL file.
     public void makeEmpty() {
         head = tail = null;
         size = 0; 
     }
 
     public boolean equals(Object obj) { 
-        if (this == obj) return true; 
+        if (this == obj) return true;
+
+        // Change this to:
+            // if (obj == null || getClass() != obj.getClass()) return false;
         if (!(obj instanceof DLL<?>)) return false;
+
+        // This is good.
         DLL<?> other = (DLL<?>) obj; 
+
+        // Everything below can be done using:
+            // return this.list.equals(other.list);
+        // This is a method that checks if lists are equal that was created in the DLL file.
+
         if (this.size != other.size) return false; 
 
         Node current1 = this.head; 
--- src/Problem2/Driverfile.java
+++ src/Problem2/Driverfile.java
@@ -2,6 +2,14 @@ package dllist;
 
 public class Driverfile {
 
+    // Where did these two methods come from?
+    // There should only be a main method.
+
+    // In the main method, define a new Deque that takes an Integer type.
+    // Use the methods you created in the Deque file to satisfy the requirements in the README file.
+
+    // I don't think outputs are necessary, but you could just print them to the console to make sure it's functional.
+
     public static void addNumbers(Deque<Integer> deque, int[] number) {
         for (int number : numbers) {
             if (number% 2 == 0) {

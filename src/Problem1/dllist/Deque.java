16c3a112daa4597e7e981ad6a76298243e0b7ee7
melanie-maira <mrp13@stmarys-ca.edu>
2024-11-22 16:15:44 -0800

    changes to deque file
     i'm having trouble implementing the DLL<T> list

---
+++ src/Problem1/dllist/Deque.java
@@ -0,0 +1,63 @@
+import java.util.Objects;
+
+import org.w3c.dom.Node;
+
+import DLL; 
+
+public class Deque<T> {
+    private DLL<T> list; 
+
+    public Deque(){
+        this.list = newDLL<>(); 
+    }
+    
+    public void addFront(T data) { // adds data to the front of collection
+           list.insert(0, data);
+    }
+
+    public void addBack(T data) { // adds data to the back of collection
+        list.append(data); 
+        } 
+    
+
+    }
+
+    public T removeFront() { // removes data from front of collection
+       if (list.isEmpty()){ 
+        throw new IllegalStateException("deque is empty");
+       }
+       DLL<T>.Node headNode = getHead().data;
+       list.delete(headNode); 
+        return data;
+        }
+
+
+
+    public void removeBack() { // removes data from the back of collection 
+        if (list.isEmpty()){ 
+            throw new IllegalStateException("deque is empty");
+        } 
+        DLL<T>.Node headNode = getNode(list.size()-1).data;
+        list.delete(headNode); 
+        }
+
+
+    public boolean isEmpty() {  // checks if collection is empty, if size is 0 then collection is considered empty
+       return size == 0;
+    }
+
+
+    public void makeEmpty() { // cancels out the data in the collection 
+        head = tail = null;
+        size = 0; 
+    }
+
+    public boolean equals(Object obj) { // checks if certain data in the collection are equal to each other
+        if (this == obj) return true; 
+        if (obj == null || getClass() != obj.getClass()) return false;
+        DLL<?> other = (DLL<?>) obj; 
+        if (this.size != other.size) return false; 
+
+        return this.list.equals(other.list); 
+    }
+
